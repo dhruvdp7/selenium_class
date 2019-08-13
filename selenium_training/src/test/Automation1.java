bd6c0415177ac535aa1ce25528ddda45735dc0e6
dhruvdp7 <46739476+dhruvdp7@users.noreply.github.com>
2019-08-13 08:58:39 +0530

    Add files via upload

--- selenium_training/src/test/Automation1.java
+++ selenium_training/src/test/Automation1.java
@@ -1,7 +1,10 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,17 +15,20 @@ public class Automation1 {
 		System.setProperty("webdriver.chrome.driver", "/home/dhruv/chromedriver");
 		//System.setProperty("webdriver.gecko.driver","/home/dhruv/geckodriver");
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://google.com");
 		
 		//google search
 		
 		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("hello world");
+		searchBox.sendKeys("wikipedia");
 		searchBox.sendKeys(Keys.RETURN);
 		
 		String title = driver.getTitle();
 		System.out.println(title);
 		
+		driver.findElement(By.linkText("Captain Marvel (film)")).click();
+		driver.findElement(By.partialLinkText("superhero")).click();
 		
 	}
 }
--- selenium_training/src/test/DoubleClick.java
+++ selenium_training/src/test/DoubleClick.java
@@ -2,22 +2,34 @@ package test;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DragAndDrop {
+public class DoubleClick {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/home/dhruv/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("https://www.w3schools.com/html/html5_draganddrop.asp");
-		WebElement from = driver.findElement(By.id("drag1"));
-		WebElement to = driver.findElement(By.xpath("//img[@id=\"drag1\"]/../following-sibling::div"));
-		Actions act = new Actions(driver);
-		act.dragAndDrop(from, to).build().perform();
+		driver.get("http://demo.guru99.com/test/simple_context_menu.html");
+		
+		Actions action = new Actions(driver);
+		WebElement Button = driver.findElement(By.xpath("//button[text()='Double-Click Me To See Alert']"));
+		action.doubleClick(Button).perform();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+		//handling alerts
+		Alert alert = driver.switchTo().alert();
+		alert.dismiss();
+		
+		
 	}
 }
